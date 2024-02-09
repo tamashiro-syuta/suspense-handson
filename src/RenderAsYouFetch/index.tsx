345ca3089673c37f54d07180be66d5b62c2d0902
tamashiro-syuta <syuta5825@gmail.com>
2024-02-09 18:13:02 +0900

    ✨ なんとなく、mui使ってアニメーション追加してみた

--- src/RenderAsYouFetch/index.tsx
+++ src/RenderAsYouFetch/index.tsx
@@ -1,4 +1,5 @@
 import { Suspense, useState } from "react";
+import { CircularProgress } from '@mui/material';
 import { Loadable } from "./Loadable";
 import { sleep } from "../sleep";
 
@@ -25,13 +26,13 @@ const RenderAsYouFetch = () => {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<CircularProgress />}>
         <DataLoader data={data1} />
       </Suspense>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<CircularProgress />}>
         <DataLoader data={data2} />
       </Suspense>
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<CircularProgress />}>
         <DataLoader data={data3} />
       </Suspense>
     </div>
